90b6747ea09e0a9a2bfeaee3fdb4c1fc112ed8b8
tomarin <ky.t.h1218@gmail.com>
2016-10-12 22:22:57 +0900

    テスト

---
+++ aisatu/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  aisatu
+//
+//  Created by 恭平 on 2016/10/01.
+//  Copyright © 2016年 恭平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func textchange(_ sender: AnyObject) {
+        text.text="変更"
+        labeltext.text="おはよう世界"
+    }
+    
+    
+    @IBOutlet weak var text: UITextField!
+    
+    @IBOutlet weak var labeltext: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
